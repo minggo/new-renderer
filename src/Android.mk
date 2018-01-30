790f74dd665d51b30ed63bcd642668e8caa8404d
minggo <ming@cocos2d-x.org>
2018-01-30 14:04:17 +0800

    fix Android build

--- src/Android.mk
+++ src/Android.mk
@@ -22,10 +22,24 @@ LOCAL_SRC_FILES := $(LOCAL_PATH)/types.cpp \
                    $(LOCAL_PATH)/gfx/CCTexture.cpp \
                    $(LOCAL_PATH)/gfx/CCTexture2D.cpp \
                    $(LOCAL_PATH)/gfx/CCVertexBuffer.cpp \
-                   $(LOCAL_PATH)/gfx/CCVertexFormat.cpp
+                   $(LOCAL_PATH)/gfx/CCVertexFormat.cpp \
+                   $(LOCAL_PATH)/renderer/BaseRenderer.cpp \
+                   $(LOCAL_PATH)/renderer/Camera.cpp \
+                   $(LOCAL_PATH)/renderer/Config.cpp \
+                   $(LOCAL_PATH)/renderer/Effect.cpp \
+                   $(LOCAL_PATH)/renderer/InputAssembler.cpp \
+                   $(LOCAL_PATH)/renderer/Light.cpp \
+                   $(LOCAL_PATH)/renderer/Model.cpp \
+                   $(LOCAL_PATH)/renderer/Pass.cpp \
+                   $(LOCAL_PATH)/renderer/ProgramLib.cpp \
+                   $(LOCAL_PATH)/renderer//Scene.cpp \
+                   $(LOCAL_PATH)/renderer/Technique.cpp \
+                   $(LOCAL_PATH)/renderer/Utils.cpp \
+                   $(LOCAL_PATH)/renderer/View.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-                    $(LOCAL_PATH)/gfx
+                    $(LOCAL_PATH)/gfx \
+                    $(LOCAL_PATH)/renderer
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/gfx
 
