3d1ec9aeac4110c801e3bb0966e1bd674f34007a
James Chen <jianhua.chen@cocos2d-x.org>
2018-02-01 10:57:07 +0800

    Remove rect deps.

--- cocos/math/CCGeometry.h
+++ cocos/math/CCGeometry.h
@@ -111,109 +111,109 @@ public:
     static const Size ZERO;
 };
 
-/**Rectangle area.*/
-class CC_DLL Rect
-{
-public:
-    /**Low left point of rect.*/
-    Vec2 origin;
-    /**Width and height of the rect.*/
-    Size  size;
-
-public:
-    /**
-    Constructor an empty Rect.
-     * @js NA
-     */
-    Rect();
-    /**
-    Constructor a rect.
-     * @js NA
-     */
-    Rect(float x, float y, float width, float height);
-    /**
-     Constructor a rect.
-     * @js NA
-     */
-    Rect(const Vec2& pos, const Size& dimension);
-    /**
-    Copy constructor.
-     * @js NA
-     * @lua NA
-     */
-    Rect(const Rect& other);
-    /**
-     * @js NA
-     * @lua NA
-     */
-    Rect& operator= (const Rect& other);
-    /**
-    Set the x, y, width and height of Rect.
-     * @js NA
-     * @lua NA
-     */
-    void setRect(float x, float y, float width, float height);
-    /**
-    Get the left of the rect.
-     * @js NA
-     */
-    float getMinX() const; /// return the leftmost x-value of current rect
-    /**
-    Get the X coordinate of center point.
-     * @js NA
-     */
-    float getMidX() const; /// return the midpoint x-value of current rect
-    /**
-    Get the right of rect.
-     * @js NA
-     */
-    float getMaxX() const; /// return the rightmost x-value of current rect
-    /**
-    Get the bottom of rect.
-     * @js NA
-     */
-    float getMinY() const; /// return the bottommost y-value of current rect
-    /**
-    Get the Y coordinate of center point.
-     * @js NA
-     */
-    float getMidY() const; /// return the midpoint y-value of current rect
-    /**
-    Get top of rect.
-     * @js NA
-     */
-    float getMaxY() const; /// return the topmost y-value of current rect
-    /**
-    Compare two rects.
-     * @js NA
-     */
-    bool equals(const Rect& rect) const;
-    /**
-    Check if the points is contained in the rect.
-     * @js NA
-     */
-    bool containsPoint(const Vec2& point) const;
-    /**
-    Check the intersect status of two rects.
-     * @js NA
-     */
-    bool intersectsRect(const Rect& rect) const;
-    /**
-    Check the intersect status of the rect and a circle.
-     * @js NA
-     */
-    bool intersectsCircle(const Vec2& center, float radius) const;
-    /**
-    Get the min rect which can contain this and rect.
-     * @js NA
-     * @lua NA
-     */
-    Rect unionWithRect(const Rect & rect) const;
-    /**Compute the min rect which can contain this and rect, assign it to this.*/
-    void merge(const Rect& rect);
-    /**An empty Rect.*/
-    static const Rect ZERO;
-};
+///**Rectangle area.*/
+//class CC_DLL Rect
+//{
+//public:
+//    /**Low left point of rect.*/
+//    Vec2 origin;
+//    /**Width and height of the rect.*/
+//    Size  size;
+//
+//public:
+//    /**
+//    Constructor an empty Rect.
+//     * @js NA
+//     */
+//    Rect();
+//    /**
+//    Constructor a rect.
+//     * @js NA
+//     */
+//    Rect(float x, float y, float width, float height);
+//    /**
+//     Constructor a rect.
+//     * @js NA
+//     */
+//    Rect(const Vec2& pos, const Size& dimension);
+//    /**
+//    Copy constructor.
+//     * @js NA
+//     * @lua NA
+//     */
+//    Rect(const Rect& other);
+//    /**
+//     * @js NA
+//     * @lua NA
+//     */
+//    Rect& operator= (const Rect& other);
+//    /**
+//    Set the x, y, width and height of Rect.
+//     * @js NA
+//     * @lua NA
+//     */
+//    void setRect(float x, float y, float width, float height);
+//    /**
+//    Get the left of the rect.
+//     * @js NA
+//     */
+//    float getMinX() const; /// return the leftmost x-value of current rect
+//    /**
+//    Get the X coordinate of center point.
+//     * @js NA
+//     */
+//    float getMidX() const; /// return the midpoint x-value of current rect
+//    /**
+//    Get the right of rect.
+//     * @js NA
+//     */
+//    float getMaxX() const; /// return the rightmost x-value of current rect
+//    /**
+//    Get the bottom of rect.
+//     * @js NA
+//     */
+//    float getMinY() const; /// return the bottommost y-value of current rect
+//    /**
+//    Get the Y coordinate of center point.
+//     * @js NA
+//     */
+//    float getMidY() const; /// return the midpoint y-value of current rect
+//    /**
+//    Get top of rect.
+//     * @js NA
+//     */
+//    float getMaxY() const; /// return the topmost y-value of current rect
+//    /**
+//    Compare two rects.
+//     * @js NA
+//     */
+//    bool equals(const Rect& rect) const;
+//    /**
+//    Check if the points is contained in the rect.
+//     * @js NA
+//     */
+//    bool containsPoint(const Vec2& point) const;
+//    /**
+//    Check the intersect status of two rects.
+//     * @js NA
+//     */
+//    bool intersectsRect(const Rect& rect) const;
+//    /**
+//    Check the intersect status of the rect and a circle.
+//     * @js NA
+//     */
+//    bool intersectsCircle(const Vec2& center, float radius) const;
+//    /**
+//    Get the min rect which can contain this and rect.
+//     * @js NA
+//     * @lua NA
+//     */
+//    Rect unionWithRect(const Rect & rect) const;
+//    /**Compute the min rect which can contain this and rect, assign it to this.*/
+//    void merge(const Rect& rect);
+//    /**An empty Rect.*/
+//    static const Rect ZERO;
+//};
 
 NS_CC_END
 
